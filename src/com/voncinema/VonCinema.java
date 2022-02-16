e458bdf39eb580d411d905bc385e6a34b157ed6e
falkgeist <falkvonfreigeist@gmail.com>
2022-02-16 13:50:11 +0100

    Implement main method for swing form

--- src/com/voncinema/VonCinema.java
+++ src/com/voncinema/VonCinema.java
@@ -9,12 +9,23 @@ public class VonCinema {
     private JComboBox selectVorstellung;
     private JSpinner spinnerAnzahlKinder;
     private JSpinner spinnerAnzahlErwachsene;
-    private JSpinner spinnerAnzahlErmäßigt;
+    private JSpinner spinnerAnzahlErmaeßigt;
     private JTextField textField1;
     private JLabel labelAnzahlKinder;
     private JLabel labelAnzahlErwachsene;
-    private JLabel labelAnzahlErmäßigt;
+    private JLabel labelAnzahlErmaeßigt;
     private JLabel labelRabattcode;
+    private JPanel tickets;
+    private JLabel labelFilm;
+    private JLabel labelVorstellung;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("VonCinema");
+        frame.setContentPane(new VonCinema().start);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
