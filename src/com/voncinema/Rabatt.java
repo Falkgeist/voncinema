929865f342ed04ab4cbf53dea52a08ad6e7f0876
vondrehleyannik <yanvondre@t-online.de>
2022-03-02 10:41:52 +0100

    Add saveToDB to Rabatt

--- src/com/voncinema/Rabatt.java
+++ src/com/voncinema/Rabatt.java
@@ -1,5 +1,9 @@
 package com.voncinema;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Rabatt {
     private int ID;
     private String name;
@@ -9,5 +13,20 @@ public class Rabatt {
         this.ID = ID;
         this.name = name;
         this.wert = wert;
+    }
+
+    public void saveToDB()
+    {
+        try {
+            Connection conn = Kinoverwaltung.setupConnection();
+            Statement stat = conn.createStatement();
+            String sql = "INSERT INTO vc_rabatt VALUES(" + name + "," + wert + ");";
+            stat.executeUpdate(sql);
+            conn.close();
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            System.err.println(e);
+        }
     }
 }
