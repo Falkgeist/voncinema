6f48c4f1b77dbbfcc813557451b3cfa66495615e
vondrehleyannik <yanvondre@t-online.de>
2022-03-02 10:36:10 +0100

    Add getlastIDFromDB to Karte

--- src/com/voncinema/Karte.java
+++ src/com/voncinema/Karte.java
@@ -1,6 +1,7 @@
 package com.voncinema;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -26,5 +27,22 @@ public class Karte {
         {
             System.err.println(e);
         }
+    }
+
+    public int getLastIdFromDB()
+    {
+        try {
+            Connection conn = Kinoverwaltung.setupConnection();
+            Statement stat = conn.createStatement();
+            String sql = "SELECT ID FROM vc_buchung ORDER BY ID DESC LIMIT 1;";
+            ResultSet rs = stat.executeQuery(sql);
+            conn.close();
+            return rs.getInt("ID");
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            System.err.println(e);
+        }
+
     }
 }
