fa42e27112cebf130aaed7609f936b5b86f7ce45
vondrehleyannik <yanvondre@t-online.de>
2022-03-02 10:11:13 +0100

    Add saveToDB to buchung

--- src/com/voncinema/Buchung.java
+++ src/com/voncinema/Buchung.java
@@ -1,5 +1,9 @@
 package com.voncinema;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Buchung {
@@ -40,5 +44,20 @@ public class Buchung {
     public void berechnePreis()
     {
 
+    }
+
+    public void saveToDB()
+    {
+        try {
+            Connection conn = Kinoverwaltung.setupConnection();
+            Statement stat = conn.createStatement();
+            String sql = "Insert into vc_buchung VALUES(" + person + "," + vorstellung + "," + karten + "," + status + ");";
+            stat.executeUpdate(sql);
+            conn.close();
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            System.err.println(e);
+        }
     }
 }
